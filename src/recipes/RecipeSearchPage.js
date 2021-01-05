59e56c9b7ce9c0f8fec7539b9ee2261294f1a32b
Bob Casazza <rcasazza@gmail.com>
2021-01-04 19:12:52 -0500

    add basic routing to pages

---
+++ src/recipes/RecipeSearchPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const RecipeSearchPage = () => {
+    return (
+        <h1>This is the Recipe Search Page</h1>
+    )
+}
---
+++ src/shopping-list/ShoppingListPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const ShoppingListPage = () => {
+    return (
+        <h1>This is the Shopping List Page</h1>
+    )
+}
