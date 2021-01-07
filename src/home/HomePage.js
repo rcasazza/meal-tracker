171a934c4d4589b93984e883b84d22543fec6d3d
Bob Casazza <rcasazza@gmail.com>
2021-01-06 19:28:18 -0500

    enable first full stack communication

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { useMeals } from '../meals';
 
 export const HomePage = () => {
+    const { meals, isLoading: isLoadingMeals, setMeals } = useMeals();
+
+    console.log(meals);
+    
     return (
         <h1>This is the Home Page</h1>
     )
---
+++ src/meals/useMeals.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+export const useMeals = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [meals, setMeals] = useState([]);
+
+
+    const loadMeals = async () => {
+        const response = await fetch('/meals');
+        const meals = await response.json();
+        setMeals(meals);
+        setIsLoading(false);
+    }
+
+    useEffect(() => {
+        loadMeals();
+    }, []);
+
+    return { isLoading, meals, setMeals };
+}
