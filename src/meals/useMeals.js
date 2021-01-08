9c8adbc85ef57fe368bc5dc86bcf25b83ee5bf99
Bob Casazza <rcasazza@gmail.com>
2021-01-07 19:26:24 -0500

    complete meals flow

--- src/meals/useMeals.js
+++ src/meals/useMeals.js
@@ -2,19 +2,24 @@ import { useState, useEffect } from 'react';
 
 export const useMeals = () => {
     const [isLoading, setIsLoading] = useState(true);
-    const [meals, setMeals] = useState([]);
-
-
-    const loadMeals = async () => {
-        const response = await fetch('/meals');
-        const meals = await response.json();
-        setMeals(meals);
-        setIsLoading(false);
-    }
+    const [rawMeals, setRawMeals] = useState([]);
 
     useEffect(() => {
+        const loadMeals = async () => {
+            const response = await fetch('/meals');
+            const rawMealsResponse = await response.json();
+            setRawMeals(rawMealsResponse);
+            setIsLoading(false);      
+        }
         loadMeals();
     }, []);
 
-    return { isLoading, meals, setMeals };
+    return { 
+        isLoading, 
+        meals: rawMeals.map(rawMeal => ({
+            ...rawMeal,
+            plannedDate: new Date(rawMeal.plannedDate)
+        })),
+        setMeals: setRawMeals,
+    };
 }
